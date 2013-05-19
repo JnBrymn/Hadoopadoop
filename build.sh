8bc878ec23ef44d20fb2cf82075e221f73e5eea4
John Berryman <jfberryman@gmail.com>
2013-05-19 16:14:08 -0400

    fixing

--- build.sh
+++ build.sh
@@ -5,18 +5,23 @@ if [ $# -eq 0 ] ; then
   exit 0
 fi
 
-name=$1
+name=${1%/}
 
+echo "Unzipping contents in ${name}/${name}In"
 pushd .
 cd ${name}/${name}In
 unzip -o *.zip
 rm *.zip
+echo "Building contents of ${name}/${name}Src"
 cd ..
 rm ${name}.jar
 rm -fr ${name}Classes
 rm -fr ${name}Out
-mkdir ${name}Classes 
-javac -classpath ${HADOOP_HOME}/hadoop*-*core*.jar -d ${name}Classes ${name}.java 
+mkdir ${name}Classes
+javac -classpath ${HADOOP_HOME}/hadoop*-*core*.jar -d ${name}Classes ${name}Src/*.java 
+echo "Jarring"
 jar -cvf ${name}.jar -C ${name}Classes/ .
+echo "Running Hadoop Job"
+echo "hadoop jar ${name}.jar org.myorg.${name} ${name}In/ ${name}Out"
 ${HADOOP_HOME}/bin/hadoop jar ${name}.jar org.myorg.${name} ${name}In/ ${name}Out
 popd
